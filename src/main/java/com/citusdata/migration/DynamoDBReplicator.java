af200be01ff00217042ee3b10b827aa97bb89ddb
EighteenOne <31286116+EighteenOne@users.noreply.github.com>
2024-07-11 13:49:39 +0500

    add info to exception

--- src/main/java/com/citusdata/migration/DynamoDBReplicator.java
+++ src/main/java/com/citusdata/migration/DynamoDBReplicator.java
@@ -174,7 +174,9 @@ public class DynamoDBReplicator {
 				if (postgresTableNamesString != null) {
 					postgresTableNames = Arrays.asList(postgresTableNamesString.split(","));
 					if (postgresTableNames.size() != dynamoTableNames.size()) {
-						throw new ParseException("Dynamo and Postgres table lists must be the same size");
+						throw new ParseException("Dynamo and Postgres table lists must be the same size."
+						+ "pt str: "+ postgresTableNamesString + " size " + postgresTableNames.size() + "list:" + postgresTableNames +";"
+						+ "dynamo str:" + dynamoTableNamesString + " size " + dynamoTableNames.size() "list:" + dynamoTableNames);
 					}
 				}
 			    for(int i = 0; i < dynamoTableNames.size(); i++) {
